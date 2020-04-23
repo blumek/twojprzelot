c42cf46670b62ea4bd1d1a17e576ac48ab6fa4ff
Blumek <blumczynski.szymon@gmail.com>
2020-04-23 11:43:45 +0200

    Initial commit

---
+++ src/main/java/pl/twojprzelot/backend/application/TwojPrzelotApplication.java
@@ -0,0 +1,13 @@
+package pl.twojprzelot.backend.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojPrzelotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwojPrzelotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/twojprzelot/backend/application/TwojPrzelotApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.twojprzelot.backend.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class TwojPrzelotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
