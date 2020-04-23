0ad5fcf3095151367040bf09b0b2b11edc7d75a8
Blumek <blumczynski.szymon@gmail.com>
2020-04-23 13:52:46 +0200

    Add Country

---
+++ src/main/java/pl/twojprzelot/backend/domain/entity/Country.java
@@ -0,0 +1,22 @@
+package pl.twojprzelot.backend.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Map;
+
+import static lombok.AccessLevel.*;
+
+@AllArgsConstructor(access = PRIVATE)
+@Builder(toBuilder = true)
+@Value
+public class Country {
+    String name;
+    String iso2Code;
+    String iso3Code;
+    int isoNumber;
+    int population;
+    Currency currency;
+    Map<Language, String> nameTranslations;
+}
---
+++ src/test/java/pl/twojprzelot/backend/domain/entity/CountryTest.java
@@ -0,0 +1,203 @@
+package pl.twojprzelot.backend.domain.entity;
+
+import com.google.common.collect.Maps;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static pl.twojprzelot.backend.domain.entity.Language.POLISH;
+
+class CountryTest {
+    private static final String NAME = "NAME";
+    private static final String ANOTHER_NAME = "ANOTHER_NAME";
+    private static final String ISO_2_CODE = "ISO_2_CODE";
+    private static final String ISO_3_CODE = "ISO_3_CODE";
+    private static final int ISO_NUMBER = 1;
+    private static final int POPULATION = 10;
+    private static final int ANOTHER_POPULATION = 20;
+    private static final String CURRENCY_NAME = "CURRENCY_NAME";
+    private static final String NAME_TRANSLATION = "NAME_TRANSLATION";
+
+    @Test
+    void builderTest_name() {
+        Country country = Country.builder()
+                .name(NAME)
+                .build();
+
+        assertEquals(NAME, country.getName());
+    }
+
+    @Test
+    void builderTest_iso2Code() {
+        Country country = Country.builder()
+                .iso2Code(ISO_2_CODE)
+                .build();
+
+        assertEquals(ISO_2_CODE, country.getIso2Code());
+    }
+
+    @Test
+    void builderTest_iso3Code() {
+        Country country = Country.builder()
+                .iso3Code(ISO_3_CODE)
+                .build();
+
+        assertEquals(ISO_3_CODE, country.getIso3Code());
+    }
+
+    @Test
+    void builderTest_isoNumber() {
+        Country country = Country.builder()
+                .isoNumber(ISO_NUMBER)
+                .build();
+
+        assertEquals(ISO_NUMBER, country.getIsoNumber());
+    }
+
+    @Test
+    void builderTest_population() {
+        Country country = Country.builder()
+                .population(POPULATION)
+                .build();
+
+        assertEquals(POPULATION, country.getPopulation());
+    }
+
+    @Test
+    void builderTest_currency() {
+        Currency currency = Currency.builder()
+                .name(CURRENCY_NAME)
+                .build();
+
+        Country country = Country.builder()
+                .currency(currency)
+                .build();
+
+        assertEquals(currency, country.getCurrency());
+    }
+
+    @Test
+    void builderTest_nameTranslations() {
+        Map<Language, String> nameTranslations = Maps.newHashMap();
+        nameTranslations.put(POLISH, NAME_TRANSLATION);
+
+        Country country = Country.builder()
+                .nameTranslations(nameTranslations)
+                .build();
+
+        assertThat(country.getNameTranslations(), hasEntry(POLISH, NAME_TRANSLATION));
+    }
+
+    @Test
+    void builderTest_toBuilder() {
+        Country country = Country.builder()
+                .name(NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(POPULATION)
+                .build();
+
+        Country anotherCountry = country.toBuilder()
+                .name(ANOTHER_NAME)
+                .population(ANOTHER_POPULATION)
+                .build();
+
+        Country expectedCountry = Country.builder()
+                .name(ANOTHER_NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(ANOTHER_POPULATION)
+                .build();
+
+        assertEquals(expectedCountry, anotherCountry);
+    }
+
+    @Test
+    void equalsTest_equalObjects() {
+        Country country = Country.builder()
+                .name(NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(POPULATION)
+                .build();
+
+        Country anotherCountry = Country.builder()
+                .name(NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(POPULATION)
+                .build();
+
+        assertEquals(country, anotherCountry);
+    }
+
+    @Test
+    void equalsTest_notEqualObjects() {
+        Country country = Country.builder()
+                .name(NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(POPULATION)
+                .build();
+
+        Country anotherCountry = Country.builder()
+                .name(ANOTHER_NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(ANOTHER_POPULATION)
+                .build();
+
+        assertNotEquals(country, anotherCountry);
+    }
+
+    @Test
+    void hashCodeTest_equalObjects() {
+        Country country = Country.builder()
+                .name(NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(POPULATION)
+                .build();
+
+        Country anotherCountry = Country.builder()
+                .name(NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(POPULATION)
+                .build();
+
+        assertEquals(country.hashCode(), anotherCountry.hashCode());
+    }
+
+    @Test
+    void hashCodeTest_notEqualObjects() {
+        Country country = Country.builder()
+                .name(NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(POPULATION)
+                .build();
+
+        Country anotherCountry = Country.builder()
+                .name(ANOTHER_NAME)
+                .iso2Code(ISO_2_CODE)
+                .iso3Code(ISO_3_CODE)
+                .isoNumber(ISO_NUMBER)
+                .population(ANOTHER_POPULATION)
+                .build();
+
+        assertNotEquals(country.hashCode(), anotherCountry.hashCode());
+    }
+}
