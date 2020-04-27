1bbd376268776cd52dc347a244247773cd1e0cf1
Blumek <blumczynski.szymon@gmail.com>
2020-04-27 14:50:57 +0200

    Add lombok's RequiredArgsConstructor to FindScheduledFlight

--- src/main/java/pl/twojprzelot/backend/usecase/FindScheduledFlight.java
+++ src/main/java/pl/twojprzelot/backend/usecase/FindScheduledFlight.java
@@ -1,16 +1,14 @@
 package pl.twojprzelot.backend.usecase;
 
+import lombok.RequiredArgsConstructor;
 import pl.twojprzelot.backend.domain.entity.ScheduledFlight;
 import pl.twojprzelot.backend.domain.port.ScheduledFlightRepository;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public class FindScheduledFlight {
     private final ScheduledFlightRepository scheduledFlightRepository;
-
-    public FindScheduledFlight(ScheduledFlightRepository scheduledFlightRepository) {
-        this.scheduledFlightRepository = scheduledFlightRepository;
-    }
 
     public Optional<ScheduledFlight> findByFlightIdentifier(String flightIdentifier) {
         return scheduledFlightRepository.findByIataNumber(flightIdentifier)
