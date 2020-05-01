11cb83255d5e4affd5d5fe93ee2cd844b863acc0
Blumek <blumczynski.szymon@gmail.com>
2020-05-01 18:36:19 +0200

    Add Airline for Aviation Edge

---
+++ src/main/java/pl/twojprzelot/backend/adapter/repository/aviation_edge/model/Airline.java
@@ -0,0 +1,45 @@
+package pl.twojprzelot.backend.adapter.repository.aviation_edge.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+class Airline {
+    private int airlineId;
+    @JsonProperty("nameAirline")
+    private String airlineName;
+
+    @JsonProperty("codeIataAirline")
+    private String airlineIataCode;
+
+    @JsonProperty("codeIcaoAirline")
+    private String airlineIcaoCode;
+
+    @JsonProperty("codeIso2Country")
+    private String countryIso2Code;
+
+    @JsonProperty("iataPrefixAccounting")
+    private String accountingIataPrefix;
+
+    private String callSign;
+    private String type;
+
+    @JsonProperty("statusAirline")
+    private String airlineStatus;
+
+    private int founding;
+
+    @JsonProperty("codeIataHub")
+    private String hubIataCode;
+
+    @JsonProperty("sizeAirline")
+    private int airlineSize;
+
+    private double ageFleet;
+}
