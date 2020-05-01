9654a1517fc8741787c6350d61b81ac286ce9af0
Blumek <blumczynski.szymon@gmail.com>
2020-05-01 18:36:56 +0200

    Add City for Aviation Edge

---
+++ src/main/java/pl/twojprzelot/backend/adapter/repository/aviation_edge/model/City.java
@@ -0,0 +1,50 @@
+package pl.twojprzelot.backend.adapter.repository.aviation_edge.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Map;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+class City {
+    @JsonProperty("cityId")
+    private int id;
+
+    @JsonProperty("nameCity")
+    private String name;
+
+    @JsonProperty("codeIataCity")
+    private String iataCode;
+
+    @JsonProperty("codeIso2Country")
+    private String countryIso2Code;
+
+    @JsonProperty("latitudeCity")
+    private double latitude;
+
+    @JsonProperty("longitudeCity")
+    private double longitude;
+
+    private String timezone;
+
+    @JsonProperty("GMT")
+    private String gmt;
+
+    private int geonameId;
+    private Translations translations;
+
+    @NoArgsConstructor
+    @Setter
+    @Getter
+    @ToString
+    static class Translations {
+        private Map<String, String> country;
+        private Map<String, String> city;
+    }
+}
