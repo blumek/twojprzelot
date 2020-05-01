d45ab2a9ab5f6d536b9b13494ced1c46f159f043
Blumek <blumczynski.szymon@gmail.com>
2020-05-01 18:36:35 +0200

    Add Airport for Aviation Edge

---
+++ src/main/java/pl/twojprzelot/backend/adapter/repository/aviation_edge/model/Airport.java
@@ -0,0 +1,62 @@
+package pl.twojprzelot.backend.adapter.repository.aviation_edge.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Map;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+class Airport {
+    @JsonProperty("airportId")
+    private int id;
+
+    @JsonProperty("nameAirport")
+    private String name;
+
+    @JsonProperty("codeIataAirport")
+    private String iataCode;
+
+    @JsonProperty("codeIcaoAirport")
+    private String icaoCode;
+
+    @JsonProperty("codeIso2Country")
+    private String countryIso2Code;
+
+    @JsonProperty("codeIataCity")
+    private String cityIataCode;
+
+    @JsonProperty("latitudeAirport")
+    private double latitude;
+
+    @JsonProperty("longitudeAirport")
+    private double longitude;
+
+    private String timezone;
+
+    @JsonProperty("GMT")
+    private String gmt;
+
+    private int geonameId;
+    private String phone;
+
+    @JsonProperty("nameCountry")
+    private String countryName;
+
+    private Translations translations;
+
+    @NoArgsConstructor
+    @Setter
+    @Getter
+    @ToString
+    static class Translations {
+        private Map<String, String> country;
+        private Map<String, String> city;
+        private Map<String, String> airport;
+    }
+}
