2feaa99f7e7f70b681b25ea8b906b3e9d74ee003
Blumek <blumczynski.szymon@gmail.com>
2020-05-12 16:29:09 +0200

    Fix spring component scan

--- src/main/java/pl/twojprzelot/backend/application/spring_app/TwojPrzelotApplication.java
+++ src/main/java/pl/twojprzelot/backend/application/spring_app/TwojPrzelotApplication.java
@@ -3,11 +3,13 @@ package pl.twojprzelot.backend.application.spring_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("pl.twojprzelot.backend.adapter.repository.database")
 @EnableJpaRepositories("pl.twojprzelot.backend.adapter.repository.database")
+@ComponentScan("pl.twojprzelot.backend")
 public class TwojPrzelotApplication {
 
     public static void main(String[] args) {
