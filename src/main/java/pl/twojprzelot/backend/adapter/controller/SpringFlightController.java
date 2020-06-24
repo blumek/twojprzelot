4ab96c018f327c203ef021896aad5ccf388ec4a8
Blumek <blumczynski.szymon@gmail.com>
2020-06-24 12:42:21 +0200

    Add SpringFlightController

---
+++ src/main/java/pl/twojprzelot/backend/adapter/controller/SpringFlightController.java
@@ -0,0 +1,46 @@
+package pl.twojprzelot.backend.adapter.controller;
+
+import com.google.common.collect.Lists;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+import static pl.twojprzelot.backend.adapter.controller.ResponseWeb.Status.ERROR;
+import static pl.twojprzelot.backend.adapter.controller.ResponseWeb.Status.SUCCESS;
+
+@RequiredArgsConstructor
+@RestController
+final class SpringFlightController {
+    private final FlightController flightController;
+
+    @GetMapping("/flights")
+    public ResponseEntity<ResponseWeb<List<FlightWeb>>> findAllBy() {
+        List<FlightWeb> foundFlights = flightController.findAll();
+        if (foundFlights.isEmpty())
+            return noResultsResponse();
+
+        return responseWithFlights(foundFlights);
+    }
+
+    private ResponseEntity<ResponseWeb<List<FlightWeb>>> noResultsResponse() {
+        return ResponseEntity.status(NOT_FOUND)
+                .body(ResponseWeb.<List<FlightWeb>>builder()
+                        .status(ERROR)
+                        .data(Lists.newArrayList())
+                        .message("No available flights")
+                        .build());
+    }
+
+    private ResponseEntity<ResponseWeb<List<FlightWeb>>> responseWithFlights(List<FlightWeb> foundFlights) {
+        return ResponseEntity.status(OK)
+                .body(ResponseWeb.<List<FlightWeb>>builder()
+                        .status(SUCCESS)
+                        .data(foundFlights)
+                        .build());
+    }
+}
---
+++ src/test/java/pl/twojprzelot/backend/adapter/controller/SpringFlightControllerTest.java
@@ -0,0 +1,86 @@
+package pl.twojprzelot.backend.adapter.controller;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.ParameterizedTypeReference;
+
+import java.util.List;
+
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+import static pl.twojprzelot.backend.adapter.controller.ResponseWeb.Status.ERROR;
+import static pl.twojprzelot.backend.adapter.controller.ResponseWeb.Status.SUCCESS;
+
+@ExtendWith(MockitoExtension.class)
+class SpringFlightControllerTest {
+    private static final int ID = 1;
+    private static final int ANOTHER_ID = 2;
+
+    @InjectMocks
+    private SpringFlightController springFlightController;
+    @Mock
+    private FlightController flightController;
+
+    @Test
+    void findAllTest_noFlightsAvailable() {
+        when(flightController.findAll())
+                .thenReturn(Lists.newArrayList());
+
+        ResponseWeb<List<FlightWeb>> expectedResponse = ResponseWeb.<List<FlightWeb>>builder()
+                .status(ERROR)
+                .data(Lists.newArrayList())
+                .message("No available flights")
+                .build();
+
+        ResponseWeb<List<FlightWeb>> foundFlights = given()
+                .standaloneSetup(springFlightController)
+        .when()
+                .get("/flights")
+        .then()
+                .contentType(JSON)
+                .status(NOT_FOUND)
+                .extract()
+                .as(new ParameterizedTypeReference<ResponseWeb<List<FlightWeb>>>() {}.getType());
+
+        assertEquals(expectedResponse, foundFlights);
+    }
+
+    @Test
+    void findAllTest_twoFlightsAvailable() {
+        FlightWeb firstFlightWeb = FlightWeb.builder()
+                .id(ID)
+                .build();
+
+        FlightWeb secondFlightWeb = FlightWeb.builder()
+                .id(ANOTHER_ID)
+                .build();
+
+        ResponseWeb<List<FlightWeb>> expectedResponse = ResponseWeb.<List<FlightWeb>>builder()
+                .status(SUCCESS)
+                .data(Lists.newArrayList(firstFlightWeb, secondFlightWeb))
+                .build();
+
+        when(flightController.findAll())
+                .thenReturn(Lists.newArrayList(firstFlightWeb, secondFlightWeb));
+
+        ResponseWeb<List<FlightWeb>> foundFlights = given()
+                .standaloneSetup(springFlightController)
+        .when()
+                .get("/flights")
+        .then()
+                .contentType(JSON)
+                .status(OK)
+                .extract()
+                .as(new ParameterizedTypeReference<ResponseWeb<List<FlightWeb>>>() {}.getType());
+
+        assertEquals(expectedResponse, foundFlights);
+    }
+}
