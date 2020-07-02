f2b23b9f06d6fdff9db5a84f0ee1bf643fd1d067
Blumek <blumczynski.szymon@gmail.com>
2020-07-02 17:41:14 +0200

    Add static data import on app start

--- src/main/java/pl/twojprzelot/backend/application/spring_app/TwojPrzelotApplication.java
+++ src/main/java/pl/twojprzelot/backend/application/spring_app/TwojPrzelotApplication.java
@@ -1,19 +1,31 @@
 package pl.twojprzelot.backend.application.spring_app;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import pl.twojprzelot.backend.usecase.*;
 
+@Slf4j
 @SpringBootApplication
 @EntityScan("pl.twojprzelot.backend.adapter.repository.database")
 @EnableJpaRepositories("pl.twojprzelot.backend.adapter.repository.database")
 @ComponentScan("pl.twojprzelot.backend")
-public class TwojPrzelotApplication {
+@RequiredArgsConstructor
+public class TwojPrzelotApplication implements CommandLineRunner {
+    private final ImportStaticData importStaticData;
 
     public static void main(String[] args) {
         SpringApplication.run(TwojPrzelotApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        importStaticData.overrideAll();
     }
 
 }
